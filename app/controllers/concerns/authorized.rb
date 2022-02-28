e40979426679a78d89f21c00ed5ceb1efe54ab55
Daniel Tinivella <dtinivella@gmail.com>
2022-02-28 20:48:28 -0300

    feat/OT151-25 Modulo Autorización

--- app/controllers/concerns/authorized.rb
+++ app/controllers/concerns/authorized.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 module Authorized
+<<<<<<< HEAD
   def admin?
     @current_user.role.name == 'admin'
+=======
+  def admin
+    render json: { errors: 'Unauthorized access' }, status: :forbidden unless admin?
+  end
+
+  def admin?
+    @current_user.role_id == 1
+>>>>>>> feat/OT151-25 Modulo Autorización
   end
 
   def owner?
