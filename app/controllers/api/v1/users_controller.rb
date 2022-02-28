436626700e6bcd7634cc60eb579ec30097e9a6b4
Daniel Tinivella <dtinivella@gmail.com>
2022-02-28 20:21:42 -0300

    feat/OT151-29 Añadir lógica de JWT

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authorize_request, except: :create
+      before_action :find_user, except: %i[create]
+
       def create
         @user = User.new(user_params)
         @user.role = Role.create_or_find_by(name: 'user', description: 'usuario de la aplicacion')
@@ -16,8 +19,14 @@ module Api
 
       private
 
+      def find_user
+        @user = User.find_by!(username: params[:_username])
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: 'User not found' }, status: :not_found
+      end
+
       def user_params
-        params.require(:user).permit(:email, :password, :first_name, :last_name)
+        params.permit(:email, :password, :first_name, :last_name)
       end
     end
   end
---
+++ spec/requests/api/v1/authentication_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Authentications', type: :request do
+  let(:valid_attributes) { attributes_for(:user) }
+  let(:valid_headers) { {} }
+
+  describe 'POST /api/v1/login' do
+    context 'should get JWT token' do
+      it 'login users' do
+        post api_v1_auth_login_url,
+             params: { user: valid_attributes },
+             headers: valid_headers, as: :json
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
+end
