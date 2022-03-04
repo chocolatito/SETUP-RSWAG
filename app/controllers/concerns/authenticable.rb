b95700114ea4c5d66a9a1212f1baaeb08734529a
Lautaro Graciani <lautarograciani2106@gmail.com>
2022-03-04 14:38:19 -0300

    Solucionada la lógica del ticket
    La lógica anterior requería un atributo del modelo de usuario que no existe en la base de datos. Volví a pensar la lógica del módulo y ya permanece en estado funcional.

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 module Authenticable
-  def current_user
-    @current_user ||= User.find_by(token: request.headers['Authorization'])
-  end
 
   def authenticate_with_token!
-    return if current_user.blank?
-
-    render json: { errors: 'Not authenticated' },
-           status: :forbidden
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
+
 end
