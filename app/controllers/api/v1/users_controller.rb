4fb1fa22066efc0ec8d28da4958f29d9660e11e2
Lautaro Graciani <lautarograciani2106@gmail.com>
2022-03-04 14:47:48 -0300

    Agregado un método set_user y serialización

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,8 @@ module Api
         render json: @users, status: :ok
       end
 
+      before_action :set_user, only: [:update]
+      before_action :ownership?, only: %i[update]
       def create
         @user = User.new(user_params)
         @user.role = Role.create_or_find_by(name: 'user', description: 'usuario de la aplicacion')
@@ -25,16 +27,16 @@ module Api
       end
 
       def update
-        @user = current_user
         if @user.update(user_params)
-          render json: @user, status: :ok
+          render json: UserSerializer.new(@user).serializable_hash.to_json
         else
-          render json: { errors: user.errors }, status: :unprocessable_entity
+          render json: @user.errors, status: :unprocessable_entity
         end
       end
 
       private
 
+<<<<<<< HEAD
       def find_user
         @user = User.find(params[:id])
       rescue ActiveRecord::RecordNotFound
@@ -44,6 +46,15 @@ module Api
       def user_params
         params.permit(:email, :password, :first_name, :last_name)
       end
+=======
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name)
+      end0
+>>>>>>> Agregado un método set_user y serialización
     end
   end
 end
