7b702c93bda73d15630eba0ed840bb6cc6d7ca5e
lautarograc <lautarograciani2106@gmail.com>
2022-03-06 20:21:15 -0300

    Fixed some code in category controller and in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :users
       post 'auth/register', to: 'users#create'
       post 'auth/login', to: 'auth#create'
       resources :categories, only: %i[create update destroy]
       post '/organization/public', to: 'organizations#create'
-      resources :users, only: [:update]
+      resources :users, only: %i[:update index]
     end
   end
 end
