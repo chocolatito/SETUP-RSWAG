965b935ed41b6f79dd1394b9aeb5967645004506
Fernando Ciballero <fernandociballero@gmail.com>
2022-03-05 21:41:31 -0300

    fix: correct request spec syntax

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -7,13 +7,11 @@ RSpec.describe 'Authentications', type: :request do
   let(:valid_headers) { {} }
 
   describe 'POST /api/v1/login' do
-    context 'should get JWT token' do
-      it 'login users' do
-        post api_v1_auth_login_url,
-             params: { user: valid_attributes },
-             headers: valid_headers, as: :json
-        expect(response.content_type).to eq('application/json; charset=utf-8')
-      end
+    it 'login users' do
+      post api_v1_auth_login_url,
+           params: { user: valid_attributes },
+           headers: valid_headers, as: :json
+      expect(response.content_type).to eq('application/json; charset=utf-8')
     end
   end
 end
