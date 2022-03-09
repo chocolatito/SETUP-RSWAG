09bbeef9b5c701c5b593ad01e6f70b9f8a8ca215
Matias <matias.devjam@gmail.com>
2022-03-09 00:13:48 -0300

    feat OT-151-42: add endpoint detail announcement

---
+++ app/serializers/announcement_serializer.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id           :bigint           not null, primary key
+#  content      :text             not null
+#  discarded_at :datetime
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  category_id  :bigint           not null
+#
+# Indexes
+#
+#  index_announcements_on_category_id   (category_id)
+#  index_announcements_on_discarded_at  (discarded_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
+class AnnouncementSerializer
+  include JSONAPI::Serializer
+  attributes :name, :content
+  belongs_to :category
+end
